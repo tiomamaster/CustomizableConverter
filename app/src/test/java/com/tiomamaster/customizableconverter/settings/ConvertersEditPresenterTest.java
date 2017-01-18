906cb86b6208c9450bab6a1082fdb8f1b157dbd0
Artyom <tioma.master@gmail.com>
2017-01-18 23:11:36 +0200

    Tests for SettingsPresenter.

---
+++ app/src/test/java/com/tiomamaster/customizableconverter/settings/ConvertersEditPresenterTest.java
@@ -0,0 +1,41 @@
+package com.tiomamaster.customizableconverter.settings;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Artyom on 18.01.2017.
+ */
+public class ConvertersEditPresenterTest {
+    @Test
+    public void handleHomePressed() throws Exception {
+
+    }
+
+    @Test
+    public void handleFabPressed() throws Exception {
+
+    }
+
+    @Test
+    public void loadConverters() throws Exception {
+
+    }
+
+    @Test
+    public void moveConverter() throws Exception {
+
+    }
+
+    @Test
+    public void deleteConverter() throws Exception {
+
+    }
+
+    @Test
+    public void enableConverter() throws Exception {
+
+    }
+
+}
--- app/src/test/java/com/tiomamaster/customizableconverter/settings/SettingsPresenterTest.java
+++ app/src/test/java/com/tiomamaster/customizableconverter/settings/SettingsPresenterTest.java
@@ -5,11 +5,17 @@ import com.tiomamaster.customizableconverter.data.SettingsRepository;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
@@ -24,4 +30,54 @@ import static org.mockito.Mockito.when;
  */
 public class SettingsPresenterTest {
 
+    @Mock private SettingsContract.SettingsView mView;
+
+    @Mock private SettingsRepository mRepository;
+
+    private SettingsPresenter mPresenter;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        mPresenter = new SettingsPresenter(mRepository, mView);
+    }
+
+    @Test
+    public void pressHomeBtnCloseSettings() {
+        mPresenter.handleHomePressed();
+
+        verify(mView).showPreviousView();
+    }
+
+    @Test
+    public void loadAndShowSummaries () {
+        String[] summaries = {"Sum1", "Sum2", "Sum3"};
+        when(mRepository.getSummaries()).thenReturn(summaries);
+
+        mPresenter.loadSummaries();
+
+        verify(mRepository).getSummaries();
+
+        verify(mView).showSummaries(summaries);
+    }
+
+    @Test
+    public void setDefaultFormDisableAllFormattingOption() {
+        when(mRepository.getDefaultForm()).thenReturn(true);
+
+        mPresenter.standardOrDefaultClicked();
+
+        verify(mView).enableFormattingOptions(false);
+    }
+
+    @Test
+    public void setStandardFormDisableGrSizeOption() {
+        when(mRepository.getStandardForm()).thenReturn(true);
+        when(mRepository.getDefaultForm()).thenReturn(false);
+
+        mPresenter.standardOrDefaultClicked();
+
+        verify(mView).enableGrSizeOption(false);
+    }
 }
